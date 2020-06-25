5b4caae5915b8434e0c2171eb9e41dbc69191f52
mishael254 <mishaelmishantoz@gmail.com>
2020-06-25 10:41:13 +0300

    register it to the main app module

--- github/src/app/app.module.ts
+++ github/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubFormComponent } from './github-form/github-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubFormComponent
   ],
   imports: [
     BrowserModule,
