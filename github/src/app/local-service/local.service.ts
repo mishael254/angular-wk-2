d99a720f0c018baae282fc4e87c1c1a954931b67
mishael254 <mishaelmishantoz@gmail.com>
2020-07-01 23:02:50 +0300

    delete httpclient from the imports in the localservice class

--- github/src/app/local-service/local.service.ts
+++ github/src/app/local-service/local.service.ts
@@ -1,24 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalService {
-  apiUrl = 'https://api.github.com/users'
-
-  constructor(private http:HttpClient) { }
-
-  getUsers()  {
-    return this.http.get(`${this.apiUrl}`)
-   
-
-  }
-
-
-  getUser(username:string) {
-
-   return this.http.get(`${this.apiUrl}/ ${username}`)
-    
-  }
+  
 }
