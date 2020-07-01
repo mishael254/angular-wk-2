f3122e21d37f808ad5a5004ab1d22fa165a26085
mishael254 <mishaelmishantoz@gmail.com>
2020-07-01 23:05:33 +0300

    delete the repository folder containing repo class

--- github/src/app/repository/github-repo.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GithubRepo } from './github-repo';
-
-describe('GithubRepo', () => {
-  it('should create an instance', () => {
-    expect(new GithubRepo()).toBeTruthy();
-  });
-});
--- github/src/app/repository/github-repo.ts
+++
@@ -1,3 +0,0 @@
-export class GithubRepo {
-    constructor(public repos_url:string) {}
-}
