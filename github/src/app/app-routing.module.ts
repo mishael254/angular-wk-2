07c8dc87a2369aff23cb0f95d304791591235840
mishael254 <mishaelmishantoz@gmail.com>
2020-07-02 15:04:29 +0300

    create a new path for user

--- github/src/app/app-routing.module.ts
+++ github/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from "../app/about/about.component";
 import { GithubComponent } from "../app/github/github.component";
+import { RepoComponent } from "../app/repo/repo.component";
+import { UserComponent } from "../app/user/user.component";
 
 
 const routes: Routes = [
   {path:'home',component:GithubComponent},
   {path:'about',component:AboutComponent},
+  {path:'user',component:UserComponent},
+  {path:'repositories',component:RepoComponent},
 ];
 
 @NgModule({
