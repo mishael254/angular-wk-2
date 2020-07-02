a44e8195d26bbf4ff13e8fa8b0624e5bbb4eb299
mishael254 <mishaelmishantoz@gmail.com>
2020-07-02 12:00:34 +0300

    new parameter apiurl

--- github/src/app/local-service/local.service.ts
+++ github/src/app/local-service/local.service.ts
@@ -17,7 +17,7 @@ export class LocalService {
       login:string;
     }
     let promise = new Promise((resolve,reject)=>{
-      this.http.get<ApiResponse>( "https://api.github.com/users/repos" + environment.access_token).toPromise().then(response=>{
+      this.http.get<ApiResponse>( environment.apiUrl + environment.access_token ).toPromise().then(response=>{
         this.user.login = response.login
         
 
