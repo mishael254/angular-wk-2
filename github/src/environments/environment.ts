7cf9fbfec624fc31de02217973687d1fe3002b75
mishael254 <mishaelmishantoz@gmail.com>
2020-07-02 09:16:00 +0300

    new api acess token in the environment .ts

--- github/src/environments/environment.ts
+++ github/src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  access_token:" 8ab7c8a01791bcca157ef65facd9dc8566cb5253 "
+  
 };
 
 /*
