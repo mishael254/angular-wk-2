bfae0fe8fa523603864cf8ea739dadf29b890f10
mishael254 <mishaelmishantoz@gmail.com>
2020-07-02 09:13:42 +0300

    import user from user class folder import local service from the local service class

--- github/src/app/user/user.component.ts
+++ github/src/app/user/user.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from "../user-class/user";
+import { LocalService } from '../local-service/local.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  user:User
 
-  constructor() { }
+  constructor(private localservice:LocalService) {
+    
+   }
 
   ngOnInit(): void {
+    this.localservice.userRequest()
+    this.user = this.localservice.user
+    
+  
   }
 
 }
