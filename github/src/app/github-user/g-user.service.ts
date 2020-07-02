bad23f4afbf8578b378cdf7afcc8dacdf98d7277
mishael254 <mishaelmishantoz@gmail.com>
2020-07-02 22:04:29 +0300

    create a new function get users which should be used to get users from the api a new api url

--- github/src/app/github-user/g-user.service.ts
+++ github/src/app/github-user/g-user.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GUserService {
+  apiUrl = 'https://api.github.com/users'
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  //first we get all users
+  getUsers()  {
+    return this.http.get (`${this.apiUrl}`)
+
+  }
+
+
+  //second we get user and their usernames
+
+  getUser(username:string)  {
+
+    return this.http.get (`${this.apiUrl}/${username}`)
+
+  }
 }
