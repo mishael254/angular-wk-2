3375d04bfe2befe3a81d45eb3aefed4b7aa45479
mishael254 <mishaelmishantoz@gmail.com>
2020-07-02 09:09:57 +0300

    import http client from angular import environment from environment import user from user class generate a new constructor private http client create a new method to fetch api data from the server called user request

--- github/src/app/local-service/local.service.ts
+++ github/src/app/local-service/local.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { User } from "../user-class/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocalService {
+
+  user : User
+  constructor (private http:HttpClient) {
+    this.user = new User ("")
+  }
+  userRequest() {
+    interface ApiResponse{
+      login:string;
+    }
+    let promise = new Promise((resolve,reject)=>{
+      this.http.get<ApiResponse>( "https://api.github.com/users/repos" + environment.access_token).toPromise().then(response=>{
+        this.user.login = response.login
+        
+
+        resolve()
+      },
+      error=>{
+        this.user.login = "404 error.."
+        
+
+        reject(error)
+      })
+    })
+    return promise
+  }
+  
   
 }
