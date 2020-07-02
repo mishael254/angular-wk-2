4d5af07ef48e0ee74f212f83cd26c31bb576cecf
mishael254 <mishaelmishantoz@gmail.com>
2020-07-03 00:15:51 +0300

    import the g service into the repocomponent

--- github/src/app/repo/repo.component.ts
+++ github/src/app/repo/repo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GUserService } from '../github-user/g-user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-repo',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repo.component.css']
 })
 export class RepoComponent implements OnInit {
+  Guser;
 
-  constructor() { }
+  constructor(private userservice:GUserService, private route:ActivatedRoute ) { }
 
   ngOnInit(): void {
+
+    this.route.params.subscribe(params =>{
+      const username = params['username']
+    
+  
+    this.userservice.getUser(username).subscribe(user => {
+      this.Guser = user
+    });
+
+      
+    });
   }
 
 }
