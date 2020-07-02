680c0300976faac7daad56a1c0fdfbea71eaf121
mishael254 <mishaelmishantoz@gmail.com>
2020-07-02 12:41:28 +0300

    create a route link to navigate between the components

--- github/src/app/app-routing.module.ts
+++ github/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from "../app/about/about.component";
+import { GithubComponent } from "../app/github/github.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:GithubComponent},
+  {path:'about',component:AboutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
